303edc3ed941710794c95c17a91947cdea331d47
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2022-08-27 08:39:15 +0700

    create searchbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/header/Header";
+import SearchBar from "./components/shared/SearchBar/SearchBar";
 
 import "./app.css";
 
@@ -7,6 +8,9 @@ const App = () => {
   return (
     <>
       <Header />
+      <div className="container !mt-[20px]">
+        <SearchBar />
+      </div>
     </>
   );
 };
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header>
-      <nav className="border-gray-200 px-4 lg:px-6 py-2.5 bg-sky-500">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+    <header className="border-gray-200 bg-sky-500">
+      <nav className="container px-4 lg:px-6 py-2.5">
+        <div className="flex flex-wrap justify-between items-center">
           <div className="flex items-center">
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
               Pokemon GO

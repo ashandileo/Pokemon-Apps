091f047a58f805d41e8f2faee837fcaa9bfe3329
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2022-08-28 17:55:42 +0700

    sticky header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="border-gray-200 bg-sky-500">
+    <header className="border-gray-200 bg-sky-500 sticky z-[12] w-full top-[0px]">
       <nav className="container px-4 lg:px-6 py-2.5">
         <div className="flex flex-wrap justify-between items-center">
           <div className="flex items-center">
